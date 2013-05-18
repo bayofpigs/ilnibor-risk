839a0b79aa6429e761d2a6b11c469630a20ca4b7
bayofpigs <Michael Zhang@MichaelZhang>
2013-05-18 19:28:17 -0400

    Test Commit Number 5

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -2,5 +2,6 @@ public class GitTestClass {
 	public static void main(String[] args) {
 		// Derp. Lemme commit
 		System.out.println("Test with github!");
+		System.out.println("My Git brings all the boys to the yard!!");
 	}
 }
