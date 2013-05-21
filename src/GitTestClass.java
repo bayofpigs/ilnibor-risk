5d7b5c2f5dcb57a5afefe84fecc7b88b134d7d37
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2013-05-20 22:39:14 -0400

    Trying to merge

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -4,8 +4,7 @@ public class GitTestClass {
 		// Another derpy commit
 		System.out.println("Test with github!");
 		System.out.println("My Git brings all the girls to the yard!");
-		System.out.println("This code is so useful");
-		System.out.println("I learned so much today");
-		System.out.println("The irish potato famine was actually caused by a lack of cabbage");
+		//sid - and they're like - it's better than yours
+		//akhil - tomato
 	}
 }
