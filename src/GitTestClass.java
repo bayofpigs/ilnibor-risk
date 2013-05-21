faf407a234a2899147d17d7700705055de071c99
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2013-05-20 22:57:01 -0400

    Reset the class

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -1,13 +1,6 @@
 public class GitTestClass {
 	public static void main(String[] args) {
-		// Derp. Lemme commit
-		// Another derpy commit
+		//Derp. I removed all the random stuff.
 		System.out.println("Test with github!");
-		System.out.println("My Git brings all the girls to the yard!");
-		//sid - and they're like - it's better than yours
-		//akhil - tomato
-		System.out.println("This code is so useful");
-		System.out.println("I learned so much today");
-		System.out.println("The irish potato famine was actually caused by a lack of cabbage");
 	}
 }
