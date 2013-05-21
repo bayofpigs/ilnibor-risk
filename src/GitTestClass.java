faf9918b0c3775322809140f8e6e506da5943bfb
Sid Senthilkumar <pikahero2@gmail.com>
2013-05-20 22:37:38 -0400

    Random Commit to test Merge functions

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -4,7 +4,8 @@ public class GitTestClass {
 		// Another derpy commit
 		System.out.println("Test with github!");
 		System.out.println("My Git brings all the girls to the yard!");
-		//sid - and they're like - it's better than yours
-		//akhil - potato
+		System.out.println("This code is so useful");
+		System.out.println("I learned so much today");
+		System.out.println("The irish potato famine was actually caused by a lack of cabbage");
 	}
 }
