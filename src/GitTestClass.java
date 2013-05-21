6ad4f50414474c3cf4a79208d1f96aeb168aded4
bayofpigs <Michael Zhang@MichaelZhang>
2013-05-21 13:14:03 -0400

    Merge Pl0x

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -3,5 +3,6 @@ public class GitTestClass {
 		//Comment2
 		System.out.println("Test with github!");
 		System.out.println("My git brings all the Iron Males to the basement of my dungeon.");
+		//Comment 69
 	}
 }
