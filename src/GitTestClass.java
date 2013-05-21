2c20d74f6fd314599cb794da35876a5ba0f4b079
Sid <pikahero2@gmail.com>
2013-05-21 12:53:54 -0400

    Changeshit

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -1,7 +1,5 @@
 public class GitTestClass {
 	public static void main(String[] args) {
-		//Comment2
 		System.out.println("Test with github!");
-		System.out.println("My git brings all the females to the yard");
 	}
 }
