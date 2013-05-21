51beeac81ec10356897550b59e5cac33148468b5
bayofpigs <Michael Zhang@MichaelZhang>
2013-05-21 15:52:47 -0400

    Merged

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -4,5 +4,6 @@ public class GitTestClass {
 		System.out.println("Test with github!");
 		System.out.println("My git brings all the Iron Males to the basement of my dungeon.");
 		//Comment 69
+
 	}
 }
