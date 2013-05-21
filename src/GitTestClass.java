cf80bc8d084d35b6a8b276ec171225faa65a8823
Sid <pikahero2@gmail.com>
2013-05-21 13:10:28 -0400

    Commencing Commit

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -2,6 +2,6 @@ public class GitTestClass {
 	public static void main(String[] args) {
 		//Comment2
 		System.out.println("Test with github!");
-		System.out.println("My git brings all the Iron Males to the yard");
+		//Comment 69
 	}
 }
