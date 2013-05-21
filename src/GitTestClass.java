e7af60f9748e259d56f37b10ea2a8c39cc0b3ee7
Sid <pikahero2@gmail.com>
2013-05-21 13:08:19 -0400

    Attempt to merge

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -1,5 +1,7 @@
 public class GitTestClass {
 	public static void main(String[] args) {
+		//Comment2
 		System.out.println("Test with github!");
+		System.out.println("My git brings all the Iron Males to the yard");
 	}
 }
