42edd0ef5d97d5faa5ec08e66a047273d1557ac9
bayofpigs <Michael Zhang@MichaelZhang>
2013-05-26 16:29:45 -0400

    Began work on GUI components

--- src/GuiFrame.java
+++ src/GuiFrame.java
@@ -1,12 +1,40 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.CardLayout;
+
+/**
+ * TODO:
+ * Implement GameBoardPanel
+ * Implement buttons on the mainmenu and have the buttons
+ * 	transition the layout to the actual gameboard
+ * 
+ * @see documentation on cardLayout 
+ * 	(here: http://docs.oracle.com/javase/tutorial/uiswing/layout/card.html)
+ * @author Michael Zhang
+ *
+ */
 public class GuiFrame extends JFrame {
+	private JPanel cards;
+	private MainMenuPanel mainMenu;
+	//private GameBoardPanel gameBoard;
+	private String MENUPANEL = "Menu";
+	private String GAMEPANEL = "Game";
+	
 	public GuiFrame() {
-		add(new MainMenuPanel());
-		setTitle("iLNibor's Risk");
+		
+		
+		setTitle("iLNibor's RISK");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(900, 700);
 		setLocationRelativeTo(null);
 		setResizable(false);
+		cards = new JPanel(new CardLayout());
+		
+		mainMenu = new MainMenuPanel();
+		cards.add(mainMenu, MENUPANEL);
+		
+		add(cards);
 	}
 	
 	public static void main(String[] args) {
--- src/MainMenuPanel.java
+++ src/MainMenuPanel.java
@@ -1,9 +1,27 @@
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+
+import java.awt.GridLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Dimension;
+
 
 public class MainMenuPanel extends JPanel {
+	private Image bg;
+	private String filedir;
+	
 	public MainMenuPanel() {
-		
+		Dimension bgsize = new Dimension(900, 700);
+		setPreferredSize(bgsize);
+		setMinimumSize(bgsize);
+		setMaximumSize(bgsize);
+		filedir = "resources/bgimage.png";
+		bg = new ImageIcon(filedir).getImage();
+		setLayout(new GridLayout());
 	}
 	
-	
+	public void paintComponent(Graphics g) {
+		g.drawImage(bg, 0, 0, null);
+	}
 }
