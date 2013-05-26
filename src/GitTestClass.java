e543e39be7a54124147e2504dd5ccd0ef764ce77
bayofpigs <Michael Zhang@MichaelZhang>
2013-05-26 16:35:53 -0400

    Updates to GUI

--- src/GitTestClass.java
+++
@@ -1,9 +0,0 @@
-public class GitTestClass {
-	public static void main(String[] args) {
-		//Comment2
-		System.out.println("Test with github!");
-		System.out.println("My git brings all the Iron Males to the basement of my dungeon.");
-		//Comment 69
-
-	}
-}
