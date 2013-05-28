0cf2ea30c67e367e604b4263664815b525d34de8
bayofpigs <Michael Zhang@MichaelZhang>
2013-05-27 22:38:32 -0400

    Updated Map and the GameBoardPanel.

--- src/GameBoardPanel.java
+++ src/GameBoardPanel.java
@@ -7,6 +7,7 @@ import java.awt.Insets;
 import java.awt.Image;
 import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.Color;
 
 public class GameBoardPanel extends JPanel{
 	private String mapImgDir;
@@ -21,12 +22,16 @@ public class GameBoardPanel extends JPanel{
 		setMinimumSize(BGSIZE);
 		setSize(BGSIZE);
 		setLayout(null);
+		this.setForeground(Color.gray);
+		this.setOpaque(true);
 		
-		mapImgDir = "resources/map.png";
+		mapImgDir = "resources/map.jpg";
 		mapImg = (new ImageIcon(mapImgDir)).getImage();
 	}
 	
 	public void paintComponent(Graphics g) {
-		g.drawImage(mapImg, 5, 0, null);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, BGSIZE.width, BGSIZE.height);
+		g.drawImage(mapImg, 15, 0, null);
 	}
 }
