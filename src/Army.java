c97c9644a394c09e5d7c030a1fd62a036bbf7ca0
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2013-05-29 09:53:01 -0400

    Updated Army

--- src/Army.java
+++ src/Army.java
@@ -1,6 +1,19 @@
+import java.awt.Color;
 import java.util.ArrayList;
 
 public class Army {
 	public ArrayList<Country> countries;
-	
+	//public ArrayList<Continent> continents;
+	public Color armyColor;
+	public String armyName;
+	public int riskCards;
+	public Army(Color color, String name){
+		armyColor = color;
+		armyName = name;
+	}
+	public int reinforcements(int riskTroops){
+		int territoryTroops = countries.size() / 3;
+		int continentTroops = 0;
+		return riskTroops + territoryTroops + continentTroops;
+	}
 }
